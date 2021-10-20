1a17ca90a95afdf827a2f555219861bd9042cd72
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2021-10-21 00:02:31 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("Hello World")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+console.log(dolarDun) 
+
+const euroDun = 11.2
+
+//euroDun = 11
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)

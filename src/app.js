9139c57d8b07465b8cd9a54fd1637a51a65ca7d9
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2021-10-21 01:26:55 +0300

    18 ekim ders sonu

--- src/app.js
+++ src/app.js
@@ -18,6 +18,6 @@ for (let i = 0; i < konutKredileri.length; i++) {
     console.log("<li>"+konutKredileri[i]+"</li>")   
     
 }       
-console.log("</ul>")
+console.log("</ul>") 
 
 console.log(konutKredileri)

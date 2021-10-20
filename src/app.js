3b034bd9acc52a07cd5fa8e39bd0fd9f4006a098
gorkemetem <79565642+gorkemetem@users.noreply.github.com>
2021-10-21 01:25:06 +0300

    18 ekim

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ let konutKredileri = ["Konut Kredi","Emlak Konut Kredisi","Kamu Konut Kredisi","
 
 console.log("<ul>")
 for (let i = 0; i < konutKredileri.length; i++) {
-    console.log("<li>"+konutKredileri[i]+"</li>") 
+    console.log("<li>"+konutKredileri[i]+"</li>")  
     
 }
 console.log("</ul>")
